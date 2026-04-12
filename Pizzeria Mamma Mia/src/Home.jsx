ed3fb71954a39d39312caadcaa10af865e4fcf48
AraOsorio <contacto.aracelliosorio@gmail.com>
2026-04-12 17:26:07 -0400

    se modifica home

--- Pizzeria Mamma Mia/src/Home.jsx
+++ Pizzeria Mamma Mia/src/Home.jsx
@@ -1,44 +1,23 @@
 import Header from "./Header";
 import CardPizza from "./Cardpizza";
+import { pizzas } from "./pizzas";
 
 const Home = () => {
   return (
     <>
       <Header />
-      
-      <div className="container mt-5">
-  <div className="row">
 
-    <div className="col-md-4">
-      <CardPizza
-        nombre="Napolitana"
-        precio={5950}
-        ingredientes={["mozzarella", "tomates", "jamón", "orégano"]}
-        imagen="https://assets.tmecosys.com/image/upload/t_web_rdp_recipe_584x480_1_5x/img/recipe/ras/Assets/48a49653c8716457eb0b2f7eb3c7d74c/Derivates/8d83d9ed4567fa15456d8eec7557e60006a15576.jpg"
-      />
-    </div>
-
-    <div className="col-md-4">
-      <CardPizza
-        nombre="Española"
-        precio={6950}
-        ingredientes={["mozzarella", "gorgonzola", "parmesano", "provolone"]}
-        imagen="https://tofuu.getjusto.com/orioneat-local/resized2/QyRsDfua3FTxx55HZ-2400-x.webp"
-      />
-    </div>
-
-    <div className="col-md-4">
-      <CardPizza
-        nombre="Pepperoni"
-        precio={6950}
-        ingredientes={["mozzarella", "pepperoni", "orégano"]}
-        imagen="https://atsloanestable.com/wp-content/uploads/2023/06/new-york-style-pizza2.jpg"
-      />
-    </div>
-
-  </div>
-</div>
-
+      <div className="d-flex justify-content-center gap-4 flex-wrap p-4">
+        {pizzas.map((pizza) => (
+          <CardPizza
+            key={pizza.id}
+            nombre={pizza.nombre}
+            precio={pizza.precio}
+            ingredientes={pizza.ingredientes}
+            imagen={pizza.imagen}
+          />
+        ))}
+      </div>
     </>
   );
 };
