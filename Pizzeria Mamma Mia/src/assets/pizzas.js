c38c45e94e24f676b8c6d36e53614e67dc46595e
AraOsorio <contacto.aracelliosorio@gmail.com>
2026-04-12 17:18:14 -0400

    se agrega pizzas.js

---
+++ Pizzeria Mamma Mia/src/assets/pizzas.js
@@ -0,0 +1,23 @@
+export const pizzas = [
+  {
+    id: 1,
+    nombre: "Napolitana",
+    precio: 5950,
+    ingredientes: ["mozzarella", "tomates", "jamón", "orégano"],
+    imagen: "URL_IMAGEN"
+  },
+  {
+    id: 2,
+    nombre: "Española",
+    precio: 6950,
+    ingredientes: ["mozzarella", "gorgonzola", "parmesano", "provolone"],
+    imagen: "URL_IMAGEN"
+  },
+  {
+    id: 3,
+    nombre: "Pepperoni",
+    precio: 6950,
+    ingredientes: ["mozzarella", "pepperoni", "orégano"],
+    imagen: "URL_IMAGEN"
+  }
+];
