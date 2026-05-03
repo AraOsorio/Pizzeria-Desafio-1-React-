c07b30c379a0f74884a033979c632e3b4baabc9c
Aracelli Osorio <contacto.aracelliosorio@gmail.com>
2026-05-03 17:05:22 -0400

    cambio

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 const Profile = () => {
   return (
-    <div className="container mt-4 text-dark">
+    <div className="container mt-3 text-dark">
       <h2 className="mb-3 text-dark">Perfil</h2>
       <p>Email: usuario@email.com</p>
       <button className="btn btn-dark">Cerrar sesión</button>
